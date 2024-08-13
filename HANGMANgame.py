946707dc22c38ca9970c444091ebbb65160966d5
kirtisinha404 <130162900+kirtisinha404@users.noreply.github.com>
2024-08-14 01:21:18 +0530

    Update HANGMANgame.py

--- HANGMANgame.py
+++ HANGMANgame.py
@@ -7,7 +7,7 @@ import random
 2:For each correct guess, the letter is filled in the corresponding dash.
 3:For each incorrect guess, a part of a stick figure is drawn, representing a man being "hanged". The figure is drawn in stages (head, body, arms, legs, etc.).
 4:The game continues until the word is completely guessed or the stick figure is fully drawn, resulting in a loss.
-5:The player haas 7 tries to guess the word
+5:The player has 7 tries to guess the word
 """
 
 def choose_word():
