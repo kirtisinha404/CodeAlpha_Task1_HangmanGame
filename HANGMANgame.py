a5f7417698a9f005c7ac0c50f3678277068d3c2b
kirtisinha404 <130162900+kirtisinha404@users.noreply.github.com>
2024-08-14 01:22:01 +0530

    Update HANGMANgame.py

--- HANGMANgame.py
+++ HANGMANgame.py
@@ -1,14 +1,4 @@
-#Project_1: HANGMAN_GAME
 import random
-
-#RULES:
-"""
-1:The goal is to guess a hidden word, one letter at a time.
-2:For each correct guess, the letter is filled in the corresponding dash.
-3:For each incorrect guess, a part of a stick figure is drawn, representing a man being "hanged". The figure is drawn in stages (head, body, arms, legs, etc.).
-4:The game continues until the word is completely guessed or the stick figure is fully drawn, resulting in a loss.
-5:The player has 7 tries to guess the word
-"""
 
 def choose_word():
     # List of possible words
